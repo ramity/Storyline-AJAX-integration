e5fd51bba50e4b3c85ce0780439204b73b22d304
lcb005 <lcb005@uark.edu>
2016-08-19 14:49:59 -0500

    Removed function format (causing issues in storyline implementation

--- user.js
+++ user.js
@@ -1,60 +1,73 @@
-function ajax_call(url,data)
-{
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function()
-  {
-    if(xhttp.readyState == 4 && xhttp.status == 200)
-    {
-      data = JSON.parse(xhttp.responseText);
+var xhttp = new XMLHttpRequest();
+var base_url = window.location.protocol + "//" + window.location.host;
 
-      if(!data.resp)
+xhttp.onreadystatechange = function()
+{
+  if(xhttp.readyState == 4 && xhttp.status == 200)
+  {
+    //converts responseText into JSON array/obj
+    data = JSON.parse(xhttp.responseText);
+
+    if(!data.resp)
+    {
+      console.log(data.error);
+    }
+
+    //second ajax request
+    var xhttptwo = new XMLHttpRequest();
+
+    xhttptwo.onreadystatechange = function()
+    {
+      if(xhttptwo.readyState == 4 && xhttptwo.status == 200)
       {
-        console.log(data.error);
-        return false;
+        data = JSON.parse(xhttptwo.responseText);
+
+        if(!data.resp)
+        {
+          console.log(data.error);
+        }
+      }
+    };
+
+    var player = GetPlayer();
+    var database_parameters = [
+      'name',
+      'course',
+      'company',
+      'grade',
+      're',
+      'state',
+      'isAhtd'
+    ];
+    var player_parameters = [
+      'AHTDName',
+      'course',
+      'NONcompany',
+      'g_oContentResults.nScore',
+      'AHTDRE',
+      'NONstate',
+      'isAHTD'
+    ];
+
+    postString = '';
+
+    for(z=0;z<database_parameters.length;z++)
+    {
+      if(database_parameters[z] == 'grade')
+      {
+        //value is hardcoded
+        postString += database_parameters[z] + "=" + g_oContentResults.nScore + "&";
       }
       else
-        return true;
+      {
+        postString += database_parameters[z] + "=" + player.GetVar(player_parameters[z]) + "&";
+      }
     }
+
+    xhttptwo.open("POST", base_url + "/dyn/course/storyline/email/ajax/use/token", true);
+    xhttptwo.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttptwo.send(postString);
   }
-  xhttp.open("POST", window.location.protocol + "//" + window.location.host + url, true);
-  if(data)
-    xhttp.send(data);
-  else
-    xhttp.send();
-}
-
-var player = GetPlayer();
-
-if(ajax_call("/dyn/course/storyline/email/ajax/get/token",0))
-{
-  var database_parameters = [
-    'name',
-    'course',
-    'company',
-    'grade',
-    're',
-    'state',
-    'isAhtd'
-  ];
-  var player_parameters = [
-    'AHTDName',
-    'course',
-    'NONcompany',
-    g_oContentResults.nScore,
-    'AHTDRE',
-    'NONstate',
-    'isAHTD'
-  ];
-
-  postString = '';
-
-  for(z=0;z<database_parameters.length;z++)
-  {
-    if(database_parameters[z] == 'grade')
-      postString += database_parameters[z] + "=" + player_parameters[z] + "&";
-    else
-      postString += database_parameters[z] + "=" + player.GetVar(player_parameters[z]) + "&";
-  }
-
-  ajax_call("/dyn/course/storyline/email/ajax/use/token",postString);
-}
+};
+xhttp.open("POST", base_url + "/dyn/course/storyline/email/ajax/get/token", true);
+xhttp.send();
